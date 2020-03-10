82db0e9e0a16f7d1cdf1631e5118d7f914cf13eb
Bishwa Karki <60760788+karkibishwa@users.noreply.github.com>
2020-03-09 20:47:15 -0400

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -4,7 +4,7 @@ resource "aws_autoscaling_group" "default" {
   min_size                  = 1
   health_check_grace_period = 300
   health_check_type         = "ELB"
-  desired_capacity          = 2
+  desired_capacity          = 1
   force_delete              = true
   launch_configuration      = "${aws_launch_configuration.default.name}"
   vpc_zone_identifier       = ["subnet-80170ddc"]
